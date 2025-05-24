a3d4e47066bcffaf90a2bd60a0e6b823f89fc610
julienjovy <julien@golpert.dev>
2025-05-24 18:13:45 +0200

    refactor(logo): center subtitles and improve visual output

--- src/Utils/Logo.php
+++ src/Utils/Logo.php
@@ -6,8 +6,9 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class Logo
 {
-    public static function render(SymfonyStyle $io, string $subtile): void
+    public static function render(SymfonyStyle $io, string $subtitle): void
     {
+        $io->newLine();
         $io->writeln("\033[38;5;46m██████╗ ███████╗██╗   ██╗██╗  ██╗\033[0m");
         $io->writeln("\033[38;5;46m██╔══██╗██╔════╝██║   ██║╚██╗██╔╝\033[0m");
         $io->writeln("\033[38;5;46m██║  ██║█████╗  ██║   ██║ ╚███╔╝\033[0m");
@@ -15,6 +16,11 @@ class Logo
         $io->writeln("\033[38;5;46m██████╔╝███████╗ ╚████╔╝ ██╔╝ ██╗\033[0m");
         $io->writeln("\033[38;5;46m██████╔╝███████╗ ╚████╔╝ ██╔╝ ██╗\033[0m");
         $io->writeln("\033[38;5;46m╚═════╝ ╚══════╝  ╚═══╝  ╚═╝  ╚═╝\033[0m");
-        $io->writeln("$subtile");
+        $io->title(self::centerSubtitle($subtitle, 33));
+    }
+
+    public static function centerSubtitle($text, $size)
+    {
+        return str_pad($text, $size, ' ', STR_PAD_BOTH);
     }
 }
