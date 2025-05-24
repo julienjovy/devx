78953b9a404290fdb550759aee5d433ae862c4b8
julienjovy <julien@golpert.dev>
2025-05-24 18:12:53 +0200

    refactor(laragon): rename to install:laragon and improve CLI display

--- src/Commands/Install/LaragonInstallCommand.php
+++ src/Commands/Install/LaragonInstallCommand.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace App\Commands\Setup;
+namespace App\Commands\Install;
 
+use App\Commands\Setup\QuestionHelper;
+use App\Utils\Shell;
+use App\Utils\StringUtils;
 use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,7 +15,7 @@ class LaragonInstallCommand extends Command
 {
     protected function configure()
     {
-        $this->setName('laragon:install');
+        $this->setName('install:laragon');
 
     }
 
@@ -34,32 +37,42 @@ class LaragonInstallCommand extends Command
 
             if (empty($assets)) {
                 $output->writeln("<error>Aucun fichier à télécharger trouvé pour la version {$version}.</error>");
+
                 return Command::FAILURE;
             }
 
             $output->writeln("✔️ Dernière version de Laragon : <info>{$version}</info>");
             $choices = [];
             foreach ($assets as $index => $asset) {
-                $choices[] = "{$asset['name']} ({$asset['size']} octets)";
+                $choices[count($choices) +1] = sprintf(
+                    '%s %10s',
+                    StringUtils::padRight($asset['name'], 50),
+                    StringUtils::humanReadableSize($asset['size'])
+                );
             }
 
             /** @var QuestionHelper $helper */
             $helper = $this->getHelper('question');
             $question = new ChoiceQuestion(
                 'Quel fichier souhaitez-vous télécharger ?',
-                $choices
+                $choices,
+                0
             );
             $selected = $helper->ask($input, $output, $question);
+
+            if (! $selected) {
+                return Command::FAILURE;
+            }
             $selectedIndex = array_search($selected, $choices);
 
-            $selectedAsset = $assets[$selectedIndex];
+            $selectedAsset = $assets[$selectedIndex - 1];
             $downloadUrl = $selectedAsset['browser_download_url'];
             $filename = $selectedAsset['name'];
 
             $output->writeln("📥 Téléchargement de <info>{$filename}</info> depuis <comment>{$downloadUrl}</comment>");
 
-            $savePath = getcwd() . DIRECTORY_SEPARATOR . $filename;
-//            die($savePath);
+            $savePath = getcwd().DIRECTORY_SEPARATOR.$filename;
+            //            die($savePath);
             file_put_contents($savePath, fopen($downloadUrl, 'r'));
 
             $output->writeln("✅ Fichier téléchargé ici : <info>{$savePath}</info>");
@@ -67,6 +80,7 @@ class LaragonInstallCommand extends Command
             return Command::SUCCESS;
         } catch (\Throwable $e) {
             $output->writeln("<error>Erreur : {$e->getMessage()}</error>");
+
             return Command::FAILURE;
         }
     }
